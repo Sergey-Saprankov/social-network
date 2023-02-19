bf8726ee1b8357a4799c10a7cb0a7aebb5926849
Sergey-Saprankov <sergey-saprankov@mail.ru>
2023-02-19 20:47:52 +0300

    added ThemeContext.ts and ThemeProvider.tsx and hook useContext

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,29 @@
 import {useAppDispatch} from "../common/hooks/appDispatch";
 import "../style/index.scss";
-import {Suspense, useEffect} from "react";
+import {Suspense, useContext, useEffect, useState} from "react";
 import {authMeTC} from "../features/auth/authSlice";
 import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "../pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "../pages/MainPage/MainPage.async";
+import {Loader} from "../common/components/Loader/Loader";
+import {Theme, ThemeContext} from "../style/theme/ThemeContext";
+import {useTheme} from "../style/theme/useTheme";
+
 
 export const App = () => {
     const dispatch = useAppDispatch();
+    const {theme, toggleTheme} = useTheme()
     useEffect(() => {
         dispatch(authMeTC());
     }, []);
     // const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
     // const status = useAppSelector(state => state.app.appStatus)
     return (
-        <div className="app">
-            <Link to={"/"}>about</Link>
-            <Link to={"/about"}>main</Link>
-            <Suspense fallback={<div>loading</div>}>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>theme</button>
+            <Link to={"/"}>main</Link>
+            <Link to={"/about"}>about</Link>
+            <Suspense fallback={<Loader/>}>
                 <Routes>
                     <Route path={"/about"} element={<AboutPageAsync/>}/>
                     <Route path={"/"} element={<MainPageAsync/>}/>
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import {App} from "./app/App";
 import {store} from "./store/store";
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
+import {ThemeProvider} from "./style/theme/ThemeProvider";
 
 render(
     <BrowserRouter><Provider store={store}>
-        <App/>
+        <ThemeProvider>
+            <App/>
+        </ThemeProvider>
     </Provider></BrowserRouter>,
 
     document.getElementById("root")
