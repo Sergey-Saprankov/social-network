2500cb76283389bd1ddef54db2678ef3bc911f8b
Sergey-Saprankov <sergey-saprankov@mail.ru>
2023-02-19 16:43:17 +0300

    added Lazy Suspence for page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,29 @@
-import { useAppDispatch } from "../common/hooks/appDispatch"
-import { Loader } from "../common/components/Loader/Loader"
-import '../style/index.scss'
-import { useAppSelector } from "../common/hooks/appSelector"
-import { useEffect } from "react"
-import {authMeTC} from '../features/auth/authSlice'
-import { Login } from "../features/auth/Login/Login"
+import {useAppDispatch} from "../common/hooks/appDispatch";
+import "../style/index.scss";
+import {Suspense, useEffect} from "react";
+import {authMeTC} from "../features/auth/authSlice";
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPageAsync} from "../pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "../pages/MainPage/MainPage.async";
 
 export const App = () => {
-    const dispatch = useAppDispatch()
+    const dispatch = useAppDispatch();
     useEffect(() => {
-        dispatch(authMeTC())
-    }, [])
-    const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
-    const status = useAppSelector(state => state.app.appStatus)
+        dispatch(authMeTC());
+    }, []);
+    // const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
+    // const status = useAppSelector(state => state.app.appStatus)
     return (
-        <div className='container'>
-            <Login />
-            {status === 'loading' && <Loader />}
+        <div className="app">
+            <Link to={"/"}>about</Link>
+            <Link to={"/about"}>main</Link>
+            <Suspense fallback={<div>loading</div>}>
+                <Routes>
+                    <Route path={"/about"} element={<AboutPageAsync/>}/>
+                    <Route path={"/"} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
+            {/*{status === 'loading' && <Loader />}*/}
         </div>
-    )
-}
+    );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
-import { render } from "react-dom";
-import {App} from './app/App'
-import { store } from "./store/store";
-import { Provider } from "react-redux";
+import {render} from "react-dom";
+import {App} from "./app/App";
+import {store} from "./store/store";
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <BrowserRouter><Provider store={store}>
+        <App/>
+    </Provider></BrowserRouter>,
 
-  document.getElementById("root")
+    document.getElementById("root")
 );
